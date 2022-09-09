023aa0a390a699204e5e83d1bcc47e5c6d48fdad
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-09-09 16:25:42 +0200

    Checkout button on cart component redirect to home page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,21 +1,24 @@
-import CartContext from "../components/CartContext";
-import { useContext } from "react";
+import CartContext from '../components/CartContext'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 function Cart() {
-  const [cart, setCart] = useContext(CartContext);
-  console.log(cart);
+	const [cart, setCart] = useContext(CartContext)
+	console.log(cart)
 
-  return (
-    <div>
-      {cart.map((item) => (
-        <li>
-          {item.title}: {`£ ${Math.floor(item.pricePerServing / 4 / 10)}`}
-        </li>
-      ))}
+	return (
+		<div>
+			{cart.map((item) => (
+				<li>
+					{item.title}: {`£ ${Math.floor(item.pricePerServing / 4 / 10)}`}
+				</li>
+			))}
+			<Link to='/'>
 
-      <button>Checkout</button>
-    </div>
-  );
+				<button>Checkout</button>
+			</Link>
+		</div>
+	)
 }
 
-export default Cart;
+export default Cart
