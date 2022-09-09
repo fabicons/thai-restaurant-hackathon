144c64d55f0737dae80cb65371037f9ea5c673b3
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-09-09 14:33:35 +0100

    Adding links to navigation and linking them to the pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,18 @@
 import { BsCart4 } from "react-icons/bs";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiFoodMenu } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 function NavBar() {
   return (
     <Nav>
+      <Link to="/home">
+        <AiOutlineHome />
+      </Link>
+      <Link to="/food">
+        <BiFoodMenu />
+      </Link>
       <Link to="/cart">
         <BsCart4 />
       </Link>
@@ -18,6 +26,8 @@ const Nav = styled.div`
   justify-content: flex-end;
   background-color: #fe951c;
   padding: 2.75vh;
+  gap: 2vh;
+
 `;
 
 export default NavBar;
