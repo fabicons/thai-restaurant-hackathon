d2d8a5ccc2fdf1886e384d8f25d6e22a04b82207
Craig Summers <73234010+AlphaPentagon@users.noreply.github.com>
2022-09-09 11:18:40 +0100

    First api call test

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -7,52 +7,29 @@ function Popular() {
   const [popular, setPopular] = useState([]);
 
   useEffect(() => {
-    getPopular();
-  }, []);
-
-  const getPopular = async () => {
-    const check = localStorage.getItem("popular");
-
-    if (check) {
-      setPopular(JSON.parse(check));
-    } else {
+    const fetchRecipes = async () => {
       const api = await fetch(
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        `https://api.spoonacular.com/recipes/complexSearch?cuisine=thai&number=22&apiKey=${process.env.REACT_APP_API_KEY}`
       );
       const data = await api.json();
 
-      localStorage.setItem("popular", JSON.stringify(data.recipes));
-      console.log(data);
-      setPopular(data.recipes);
-      console.log(data.recipes);
-    }
-  };
+      setPopular(data.results);
+      console.log(data.results);
+    };
+    fetchRecipes();
+  }, []);
 
   return (
     <div>
       <Wrapper>
-        <h3>Trending</h3>
-        <Splide
-          options={{
-            perPage: 4,
-            arrows: false,
-            pagination: false,
-            drag: "free",
-            gap: "5rem",
-          }}
-        >
-          {popular.map((recipe) => {
-            return (
-              <SplideSlide key={recipe.id}>
-                <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
-                </Card>
-              </SplideSlide>
-            );
-          })}
-        </Splide>
+        <h3>Our menu</h3>
+        {popular ? (
+          popular.map((recipe) => {
+            return <p>{recipe.title}</p>;
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
       </Wrapper>
     </div>
   );
